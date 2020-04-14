a0c1f469f8bfec363e5630d8e8492ee156c70a51
idanreicher <idanreicher@gmail.com>
2020-04-14 10:07:06 -0400

    charts

--- js/arrays_collections_charts/charts/sets_charts/js/page.js
+++ js/arrays_collections_charts/charts/sets_charts/js/page.js
@@ -14,62 +14,117 @@ const monthlySales = new Set();
 const monthlyLabels = new Set();
 
 function addSale() {
-    monthlySales.add(newAmount.value);
+    monthlySales.add(parseInt(newAmount.value));
     monthlyLabels.add(newMonth.value);
-    console.log(monthlySales);
+
+    yearlyTotal = 0;
+
+    monthlySalesChart.data.datasets.forEach((dataset)=>{
+        dataset.data= [];
+    });
+    deptSalesChart.data.datasets.forEach((dataset)=>{
+        dataset.data = [];
+    });
+
+    for(let amount of monthlySales){
+
+        yearlyTotal += amount;
+        yearlyLabel.innerHTML = yearlyTotal;
+
+        monthlySalesChart.data.datasets.forEach((dataset)=>{
+            dataset.data.push(amount);
+        });
+        deptSalesChart.data.datasets.forEach((dataset)=>{
+            dataset.data.push(amount);
+        });
+
+    }
+
+    monthlySalesChart.data.labels = Array.from(monthlyLabels);
+    deptSalesChart.data.labels = Array.from(monthlyLabels);
+    //console.log(monthlySalesChart.data);
+    //  console.log(deptSalesChart.data);
+    monthlySalesChart.update();
+    deptSalesChart.update();
+    
 }
 
 function deleteVal() {
-    monthlySales.delete('1223');
-    console.log(monthlySales);
+
+    monthlySalesChart.data.datasets =[];
+    monthlySalesChart.data.labels = [];
+    deptSalesChart.data.datasets = [];
+    deptSalesChart.data.labels = [];
+    monthlySalesChart.update();
+    deptSalesChart.update();
+
 }
 
-// Code goes here
+
+function addTotal() {
+
+    yearlyTotal = 0;
+
+    for(let amount of monthlySales){
+
+        yearlyTotal += amount;
+        yearlyLabel.innerHTML = yearlyTotal;
+
+        monthlySalesChart.data.datasets.forEach((dataset)=>{
+            dataset.data.push(amount);
+        });
+        deptSalesChart.data.datasets.forEach((dataset)=>{
+            dataset.data.push(amount);
+        });
+
+    }
+    
+}
 
 // Bar chart
-// var monthlySalesChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: monthlyLabels,
-//         datasets: [{
-//             label: '# of Sales',
-//             data: monthlySales,
-//             backgroundColor: [
-//                 'rgba(238, 184, 104, 1)',
-//                 'rgba(75, 166, 223, 1)',
-//                 'rgba(239, 118, 122, 1)',
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+var monthlySalesChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# of Sales',
+            data: [],
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
 
 // Pie Chart
-// var deptSalesChart = new Chart(pieCtx, {
-//     type: 'pie',
-//     data: {
-//         labels: deptLabels,
-//         datasets: [{
-//             label: '# of Sales',
-//             data: deptSales,
-//             backgroundColor: [
-//                 'rgba(238, 184, 104, 1)',
-//                 'rgba(75, 166, 223, 1)',
-//                 'rgba(239, 118, 122, 1)',
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
+var deptSalesChart = new Chart(pieCtx, {
+    type: 'pie',
+    data: {
+        labels: [],
+        datasets: [{
+            label: '# of Sales',
+            data: [],
+            backgroundColor: [
+                'rgba(238, 184, 104, 1)',
+                'rgba(75, 166, 223, 1)',
+                'rgba(239, 118, 122, 1)',
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
         
-//     }
-// })
+    }
+})
